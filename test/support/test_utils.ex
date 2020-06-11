853b33a977a7ab18cd50de5417d4f165f75bc213
Léonard Besson <leonard.besson@gmail.com>
2020-06-11 13:34:13 +0200

    Update test macros to also test the non safe API

--- test/support/test_utils.ex
+++ test/support/test_utils.ex
@@ -6,8 +6,10 @@ defmodule Bincode.TestUtils do
     quote do
       test "#{inspect(unquote(type))} serialization (#{inspect(unquote(input))})" do
         assert {:ok, serialized} = Bincode.serialize(unquote(input), unquote(type))
+        assert Bincode.serialize!(unquote(input), unquote(type)) == serialized
         assert serialized == unquote(output)
         assert Bincode.deserialize(serialized, unquote(type)) == {:ok, {unquote(input), ""}}
+        assert Bincode.deserialize!(serialized, unquote(type)) == {unquote(input), ""}
       end
     end
   end
@@ -16,7 +18,9 @@ defmodule Bincode.TestUtils do
     quote do
       test "#{inspect(unquote(type))} serialization fail (#{inspect(unquote(input))})" do
         assert {:error, _} = Bincode.serialize(unquote(input), unquote(type))
+        assert_raise(ArgumentError, fn -> Bincode.serialize!(unquote(input), unquote(type)) end)
         assert {:error, _} = Bincode.deserialize(unquote(output), unquote(type))
+        assert_raise(ArgumentError, fn -> Bincode.deserialize!(unquote(output), unquote(type)) end)
       end
     end
   end
