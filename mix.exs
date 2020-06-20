bff6d6206f3efcc3574b27edfabf15e483b3b585
Léonard Besson <leonard.besson@gmail.com>
2020-06-20 18:08:15 +0200

    v0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bincode.MixProject do
   def project do
     [
       app: :bincode,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
